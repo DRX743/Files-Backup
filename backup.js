c13cb5b6865701a5f20674b2f46a949d1308b3b3
RedaMeziouni <meziounidb@gmail.com>
2023-04-05 14:59:27 +0200

    Adding the Discord Webhook to get the backup result on a discord server

--- backup.js
+++ backup.js
@@ -2,8 +2,23 @@
 require("dotenv").config();
 const CronJob = require("cron").CronJob;
 const Rsync = require("rsync");
+const https = require("https");
 
-// The value of process.platform will be:
+process.title = "node-backup-script";
+
+// Will be true if there is a Discord WEBHOOK_ID set in the `.env` file
+const useDiscord = !!process.env.WEBHOOK_ID;
+
+const options = {
+  hostname: "discord.com",
+  path: `/api/webhooks/${process.env.WEBHOOK_ID}`,
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+// process.platform will be:
 // Windows: win32
 // Mac: darwin
 // Ubuntu: linux
@@ -16,32 +31,49 @@ rsync = new Rsync()
   .flags("a")
   // Reads from the `.env` file in the project directory
   .source(process.env.SOURCE_DIR)
-  .destination(process.env.DESTINATION_DIR); 
+  .destination(process.env.DESTINATION_DIR);
 
 const job = new CronJob(
   // Run this function once every minute
+  // corn string syntax
   process.env.CRON_STRING,
   () => {
     rsync.execute((error, code, cmd) => {
       let result;
       if (error) {
-        // Check README link for the list of rsync status codes
         result = `Code ${code} ${error?.message}`;
       } else {
         result = "Backup complete";
       }
 
       const currentDate = new Date().toISOString();
-      // Write log to the console, or will be redirected to a nohup.out file if using nohup
+      // Write log to the console, or will be redirected to a
+      // nohup.out file if using nohup
       process.stdout.write(`${currentDate}: ${result}\n`);
+
+      // Only sends the request if WEBHOOK_ID is defined
+      if (useDiscord) {
+        // Send the request to Discord with the configured options
+        const req = https.request(options, (res) => {
+          // do nothing with Discord response
+        });
+
+        // Discord requires a { content: string } shape for posting messages
+        req.write(
+          JSON.stringify({
+            content: result,
+          })
+        );
+
+        req.end();
+      }
     });
   },
   null,
   true,
-  // Add time zone
+  // Add a time zone
   "Europe/Berlin"
 );
 
 // Start the cronjob
 job.start();
-  
